f262b047e4e46b1210f1597e22ef19f0f9f5da78
hamgod420 <andyjyan@gmail.com>
2026-01-15 02:27:31 -0500

    fix: scripts

--- next.config.js
+++ next.config.js
@@ -1,34 +1,30 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   pageExtensions: ['page.js', 'api.js'],
-  webpack(config, { isServer }) {
-    // Run custom scripts
-    if (isServer) {
-      require('./scripts/generate-sitemap');
-      require('./scripts/draco');
-    }
 
-    // Import `svg` files as React components
+  webpack(config) {
+    // ✅ Import `svg` files as React components (unless `?url` is used)
     config.module.rules.push({
       test: /\.svg$/,
       resourceQuery: { not: [/url/] },
       use: [{ loader: '@svgr/webpack', options: { svgo: false } }],
     });
 
-    // Import videos, models, hdrs, and fonts
+    // ✅ Import videos, models, hdrs, and fonts
     config.module.rules.push({
       test: /\.(mp4|hdr|glb|woff|woff2)$/i,
       type: 'asset/resource',
     });
 
-    // Force url import with `?url`
+    // ✅ Force url import with `?url`
     config.module.rules.push({
       resourceQuery: /url/,
       type: 'asset/resource',
     });
 
-    // Import `.glsl` shaders
+    // ✅ Import `.glsl` shaders as source strings
     config.module.rules.push({
       test: /\.glsl$/,
       type: 'asset/source',
@@ -37,3 +33,5 @@ module.exports = {
     return config;
   },
 };
+
+module.exports = nextConfig;
